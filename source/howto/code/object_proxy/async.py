72d435ff3223e3bbcedddecd4d313af8c6d37b51
Vignesh Venkatasubramanian Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-08-08 21:44:59 +0200

    object proxy docs almost complete

--- source/howto/code/object_proxy/async.py
+++ source/howto/code/object_proxy/async.py
@@ -2,8 +2,7 @@ from hololinked.client import ObjectProxy
 
 spectrometer_proxy = ObjectProxy(instance_name='spectrometer', 
                                 protocol='IPC', async_mixin=True)
- 
-# synchronous call is always available
+# synchronous call is always available, not optional
 spectrometer_proxy.serial_number = 'USB2+H15897'
 spectrometer_proxy.invoke_action('connect', trigger_mode=2, integration_time=1000)
 spectrometer_proxy.set_properties(
@@ -13,79 +12,43 @@ spectrometer_proxy.set_properties(
 spectrometer_proxy.disconnect()
 
 
+#----------------------------
+# async calls example
 import asyncio
-asyncio.run(spectrometer_proxy.async_invoke_action('connect', trigger_mode=2, integration_time=1000))
-asyncio.run(spectrometer_proxy.async_set_properties(
+# invoke action
+asyncio.run(spectrometer_proxy.async_invoke_action('connect', 
+                                trigger_mode=2, integration_time=1000))
+# get multiple properties
+asyncio.run(spectrometer_proxy.async_write_multiple_properties(
     integration_time=100, 
     nonlinearity_correction=False
 ))
-asyncio.run(spectrometer_proxy.async_get_properties(names=["integration_time", "trigger_mode"]))
-asyncio.run(spectrometer_proxy.async_get_property('serial_number'))
+# set multiple properties
+asyncio.run(spectrometer_proxy.async_read_multiple_properties(
+    names=["integration_time", "trigger_mode"]))
+# get single property
+asyncio.run(spectrometer_proxy.async_read_property('serial_number'))
+# set single property
+asyncio.run(spectrometer_proxy.async_write_property('serial_number', 
+                                                'USB2+H15897'))
 
-# both are the same
+#----------------------------
+# running multiple clients
+async def async_example(spectrometer : ObjectProxy):
+    await spectrometer.async_invoke_action("start_acquisition")
+    for i in range(1000):
+        await spectrometer.async_read_property("last_intensity")
+        await asyncio.sleep(0.025)
+    await spectrometer.async_invoke_action("stop_acquisition")
 
-# same applied to getting property
-print(spectrometer_proxy.serial_number) # prints 'USB2+H15897'
-print(spectrometer_proxy.get_property('serial_number')) # prints 'USB2+H15897'
-
-# with keyword arguments
-spectrometer_proxy.connect(trigger_mode=2, integration_time=1000)
-spectrometer_proxy.disconnect()
-# with non keyword arguments
-spectrometer_proxy.connect(2, 1000)
-spectrometer_proxy.disconnect()
-# with both non-keyword and keyword arguments
-spectrometer_proxy.connect(2, integration_time=1000) 
-
-
-# using invoke_action
-spectrometer_proxy.connect()
-spectrometer_proxy.invoke_action('disconnect')
-# keyword arguments
-
-spectrometer_proxy.invoke_action('disconnect')
-# non keyword arguments
-spectrometer_proxy.invoke_action('connect', 2, 1000)
-spectrometer_proxy.invoke_action('disconnect')
-# with both non-keyword and keyword arguments
-spectrometer_proxy.invoke_action('connect', 2, integration_time=1000)
-spectrometer_proxy.invoke_action('disconnect')
-
-# set and get multiple properties
-print(spectrometer_proxy.get_properties(names=["integration_time", "trigger_mode"]))
-
-print(spectrometer_proxy.get_properties(names=["state", "nonlinearity_correction", 
-                                    "integration_time", "trigger_mode"]))
-
-# oneway call
-spectrometer_proxy.invoke_action('connect', oneway=True, trigger_mode=2, integration_time=1000)
-spectrometer_proxy.invoke_action('disconnect', oneway=True)
-# non keyword arguments
-spectrometer_proxy.invoke_action('connect', 2, 1000, oneway=True)
-# set properties one way
-spectrometer_proxy.set_properties(
-    integration_time=100, 
-    nonlinearity_correction=False,
-    oneway=True
-)
-print(spectrometer_proxy.get_properties(names=["state", "nonlinearity_correction", 
-                                    "integration_time", "trigger_mode"]))
-# set single property one way
-spectrometer_proxy.set_property('integration_time', 100, oneway=True)
-
-# no block calls
 spectrometer1_proxy = ObjectProxy(instance_name='spectrometer1', 
-                                protocol='IPC')
+                                protocol='IPC', async_mixin=True)
 spectrometer2_proxy = ObjectProxy(instance_name='spectrometer2', 
-                                protocol='IPC')
-reply_id1 = spectrometer1_proxy.invoke_action('connect', noblock=True, trigger_mode=2, integration_time=1000)
-reply_id2 = spectrometer2_proxy.invoke_action('connect', noblock=True, trigger_mode=2, integration_time=1000)
-# say connecting take 2 seconds per spectrometer
-spectrometer1_proxy.read_reply(reply_id1) 
-spectrometer2_proxy.read_reply(reply_id2)  
+                                protocol='IPC', async_mixin=True)
+spectrometer3_proxy = ObjectProxy(instance_name='spectrometer3', 
+                                protocol='IPC', async_mixin=True)
 
-reply_id3 = spectrometer1_proxy.invoke_action('disconnect')
-reply_id4 = spectrometer2_proxy.invoke_action('disconnect')
-# say disconnecting take 1 second per spectrometer
-spectrometer1_proxy.read_reply(reply_id3, timeout=50) # milliseconds
-# can raise TimeoutError
+asyncio.get_event_loop().run_until_complete(asyncio.gather(
+    *[async_example(spectrometer) for spectrometer in [
+        spectrometer1_proxy, spectrometer2_proxy, spectrometer3_proxy]]
+))
